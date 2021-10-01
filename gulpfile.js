a4840e65d4460eeead0c11abbd0e10bcdfd5f3cb
Filippa <filippabellange@live.se>
2021-10-01 18:45:34 +0200

    remove commentary

--- gulpfile.js
+++ gulpfile.js
@@ -27,32 +27,3 @@ gulp.task('watch', function(){
     gulp.watch('scss/*.scss', ['sass']);
     gulp.watch('css/**/*.css', ['minify']);
 });
-
-
-
-
-// var gulp = require("gulp");
-// const concatCss = require("gulp-concat-css");
-// const cssNano = require("gulp-cssnano")
-// var sass = require("gulp-sass");
-
-// gulp.task("compile", function () {
-//     return gulp
-//     .src("scss/*.scss")
-//     .pipe(sass())
-//     .pipe (concatCss("main.css"))
-//     .pipe(gulp.dest("css"));
-// });
-
-// gulp.task('minify', function minifyFunc() {
-//     return gulp.src('./css/main.css')
-//       .pipe(cssNano())
-//       .pipe (concatCss("main.css"))
-//       .pipe(gulp.dest('css'));
-//   });
-
-// gulp.task("watch-scss", function () {
-//     gulp.watch("scss/*.scss", gulp.series("compile", "minify"));
-// });
-
-// gulp.task("default", gulp.series("compile", "watch-scss"), function () {});
